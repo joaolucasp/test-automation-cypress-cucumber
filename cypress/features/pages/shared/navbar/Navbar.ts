995bde9bda4532c4f44cfa2dc0af16243620fbf0
João Lucas Pereira <j.lucaspereira@usp.br>
2024-10-25 10:51:26 -0300

    feat(navbar-component): criação da lógica do component objects da navbar

---
+++ cypress/features/pages/shared/navbar/Navbar.ts
@@ -0,0 +1,45 @@
+// Locators
+import { NavbarLocators } from '@features/pages/shared/navbar/locators/NavbarLocators';
+
+export class Navbar {
+  private readonly locators = NavbarLocators;
+
+  constructor() {}
+
+  public static clickOnOurProducts(): void {
+    cy.get(NavbarLocators.navigationLinks.ourProducts).click();
+  }
+
+  public static clickOnSpecialOffer(): void {
+    cy.get(NavbarLocators.navigationLinks.specialOffer).click();
+  }
+
+  public static clickOnPopularItems(): void {
+    cy.get(NavbarLocators.navigationLinks.popularItems).click();
+  }
+
+  public static clickOnContactUs(): void {
+    cy.get(NavbarLocators.navigationLinks.contactUs).click();
+  }
+
+  public static searchProduct(product: string) {
+    Navbar.clickOnSearchProductLink();
+    Navbar.typeSearchProduct(product);
+  }
+
+  private static clickOnSearchProductLink(): void {
+    cy.get(NavbarLocators.searchProduct.searchLink).click();
+  }
+
+  private static typeSearchProduct(product: string): void {
+    cy.get(NavbarLocators.searchProduct.searchInput).type(product);
+  }
+
+  public static clickOnProfileLink(): void {
+    cy.get(NavbarLocators.profileLink).click();
+  }
+
+  public static clickOnCartLink(): void {
+    cy.get(NavbarLocators.cartLink).click();
+  }
+}
---
+++ cypress/features/pages/shared/navbar/locators/NavbarLocators.ts
@@ -0,0 +1,23 @@
+export const NavbarLocators = {
+  navigationLinks: {
+    ourProducts: 'nav ul li:nth-child(8)',
+    specialOffer: 'nav ul li:nth-child(7)',
+    popularItems: 'nav ul li:nth-child(6)',
+    contactUs: 'nav ul li:nth-child(5)',
+  },
+
+  searchProduct: {
+    searchLink: '#search',
+    searchInput: '#search input',
+    
+    searchResultsPopUp: {
+      viewAllButton: '.searchPopUp .searchPopUp .top6Products a.viewAll',
+  
+      // The real index is N + 2, where N is the index of the product in the search results.
+      productCard: (index: number) => `.searchPopUp .searchPopUp .top6Products a.product:nth-child(${index + 2})`
+    }
+  },
+
+  profileLink: 'menuUserLink',
+  cartLink: 'shoppingCartLink',
+};
