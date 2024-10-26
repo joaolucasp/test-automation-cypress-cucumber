4a250d853b8048fe5c569e465cd77aa804ce66f6
João Lucas Pereira <j.lucaspereira@usp.br>
2024-10-26 10:46:23 -0300

    feat(cart-tooltip): criação de métodos para obtenção dos produtos presentes no carrinho (tooltip)

--- cypress/features/components/cart-tooltip/CartTooltip.ts
+++ cypress/features/components/cart-tooltip/CartTooltip.ts
@@ -1,6 +1,9 @@
 // Locators
 import { CartTooltipLocators } from '@features/components/cart-tooltip/locators/CartTooltipLocators';
 
+// Types
+import { IProductResume } from '@features/pages/shared/types/product/IProductResume';
+
 export class CartTooltip {
   private static readonly locators = CartTooltipLocators;
 
@@ -16,4 +19,33 @@ export class CartTooltip {
     return cy.getValue(CartTooltip.locators.totalPrice);
   }
 
+  public static getAllProductsFromCart(): Cypress.Chainable<IProductResume[]> {
+    return CartTooltip.getTotalItems().then((totalItems) => {
+      const allProductsInCart: IProductResume[] = [];
+
+      for (let i = 0; i < totalItems; i++) {
+        const productResumePromise = CartTooltip.getProductFromCart(i);
+
+        productResumePromise.then((productResume) => {
+          allProductsInCart.push(productResume);
+          cy.wrap(allProductsInCart).as('allProductsInCart');
+        });
+      }
+
+      return cy.get('@allProductsInCart').then((allProductsInCart) => {
+        return allProductsInCart as unknown as IProductResume[];
+      });
+    });
+  }
+
+  public static getProductFromCart(index: number): Cypress.Chainable<IProductResume> {
+    return cy.getValue(CartTooltip.locators.product(index).productName).then((productName) => {
+      return cy.getValue(CartTooltip.locators.product(index).productPrice).then((productPrice) => {
+        return {
+          productName,
+          productPrice
+        };
+      });
+    });
+  }
 }
