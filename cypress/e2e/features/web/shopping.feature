b338eaff514814fa2ade7eb1084159b965e70ce7
João Lucas Pereira <j.lucaspereira@usp.br>
2024-10-26 14:40:40 -0300

    feat(search-product): finalizado a criação dos testes automatizados planejados

--- cypress/e2e/features/web/shopping.feature
+++ cypress/e2e/features/web/shopping.feature
@@ -3,6 +3,7 @@ Feature: Shopping Cart Functionalities
   Background:
     Given the user is on the homepage
 
+  @search-product
   Scenario Outline: User searches for a product and sees relevant results
     When the user searches for "<productName>"
     Then the user should see a list of products displayed
@@ -14,13 +15,15 @@ Feature: Shopping Cart Functionalities
     | laptop      |
     | speaker     |
 
+  @search-product
   Scenario: User searches for a product that does not exist
     When the user searches for "ABCDEFGH!@#$%^&*()"
     Then the user should see a message indicating that no products were found
 
+  @add-product-to-cart
   Scenario Outline: Add a product to the cart with a specific quantity
     When the user searches for "<productName>"
-    And the user adds "<quantity>" of the first product to the cart
+    And the user adds "<quantity>" quantity of the product to the cart
     Then the user should see "<quantity>" of "<productName>" added to the cart
 
   Examples:
@@ -28,3 +31,13 @@ Feature: Shopping Cart Functionalities
     | HP STREAM - 11-D020NR LAPTOP          | 1        |
     | HP S9500 BLUETOOTH WIRELESS SPEAKER   | 2        |
     | HP PRO TABLET 608 G1                  | 3        |
+
+  @checkout
+  Scenario: Validate the products added to the cart on the checkout page
+    When the user searches for "HP Z3600 WIRELESS MOUSE"
+    And the user adds "1" quantity of the product to the cart
+    And the user searches for "BEATS STUDIO 2 OVER-EAR MATTE BLACK HEADPHONES"
+    And the user adds "3" quantity of the product to the cart
+    And the user navigates to the checkout page
+    Then the user should see "1" quantity of "HP Z3600 WIRELESS MOUSE" on checkout screen
+    And the user should see "3" quantity of "BEATS STUDIO 2 OVER-EAR MATTE BLACK HEADPHONES" on checkout screen
--- cypress/e2e/steps/web/shopping.ts
+++ cypress/e2e/steps/web/shopping.ts
@@ -2,6 +2,7 @@
 import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 
 // Page Objects
+import { Checkout } from '@features/pages/checkout/page-objects-model/Checkout';
 import { ProductList } from '@features/pages/product-list/page-objects-model/ProductList';
 import { ProductDetails } from '@features/pages/product-details/page-objects-model/ProductDetails';
 
@@ -10,6 +11,7 @@ import { Navbar } from '@features/components/navbar/Navbar';
 import { CartTooltip } from '@features/components/cart-tooltip/CartTooltip';
 
 // Assertions
+import { CheckoutAssertions } from '@features/pages/checkout/assertions/CheckoutAssertions';
 import { ProductListAssertions } from '@features/pages/product-list/assertions/ProductListAssertions';
 import { CartTooltipAssertions } from '@features/components/cart-tooltip/assertions/CartTooltipAssertions';
 
@@ -48,7 +50,7 @@ Then("the user should see a message indicating that no products were found", () 
 /* ------------------------------------------- */
 
 /* Scenario for adding a product to the cart */
-When('the user adds {string} of the first product to the cart', (quantity: string) => {
+When('the user adds {string} quantity of the product to the cart', (quantity: string) => {
   const productListPage = new ProductList();
   productListPage.viewProduct(0);
 
@@ -65,6 +67,25 @@ Then('the user should see {string} of {string} added to the cart', (quantity: st
     }
 
     CartTooltipAssertions.validateIfProductAddedToCart(product, allProductsInCart);
+  });
+});
+/* ------------------------------------------- */
+
+/* Scenario for validate the products added too the cart on the checkoutpage */
+When('the user navigates to the checkout page', () => {
+  Navbar.goToCheckoutPage();
+});
+
+Then('the user should see {string} quantity of {string} on checkout screen', (quantity: string, productName: string) => {
+  const checkoutPage = new Checkout();
+
+  checkoutPage.getAllProductsInCart().then((allProductsInCart) => {
+    const product = {
+      productName,
+      quantity: parseInt(quantity)
+    }
+
+    CheckoutAssertions.validateIfProductAddedToCart(product, allProductsInCart);
   });
 });
 /* ------------------------------------------- */
