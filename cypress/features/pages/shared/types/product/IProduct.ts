e316affaee900404dba8f6146de77c08499f06d8
João Lucas Pereira <j.lucaspereira@usp.br>
2024-10-26 11:28:11 -0300

    wip(assertions): criação do arquivo de assertions para o componente de cart tooltip

--- cypress/features/pages/shared/types/product/IProduct.ts
+++ cypress/features/pages/shared/types/product/IProduct.ts
@@ -1,6 +1,6 @@
 export interface IProduct {
   productName: string;
-  productPrice: string;
-  productDescription: string;
+  productPrice?: string;
+  productDescription?: string;
   quantity: number;
 };
